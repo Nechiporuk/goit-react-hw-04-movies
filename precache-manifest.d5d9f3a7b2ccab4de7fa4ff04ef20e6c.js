05b6796e86b6c4dfc7d62ef25bf4356b69786187
Maxim Nechiporuk <nechiporukmr@gmail.com>
2019-11-20 14:03:15 +0200

    Updates

---
+++ precache-manifest.d5d9f3a7b2ccab4de7fa4ff04ef20e6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3340c79bb7e2029b73aef1fdde2c8eba",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "bdba196b09fdae207a47",
+    "url": "/goit-react-hw-04-movies/static/css/main.4ae3599f.chunk.css"
+  },
+  {
+    "revision": "010421de84d587fa5ce9",
+    "url": "/goit-react-hw-04-movies/static/js/2.78de359e.chunk.js"
+  },
+  {
+    "revision": "bdba196b09fdae207a47",
+    "url": "/goit-react-hw-04-movies/static/js/main.1c74d5a8.chunk.js"
+  },
+  {
+    "revision": "a7cd2099343d2842d2c4",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.2c61838f.js"
+  }
+]);
